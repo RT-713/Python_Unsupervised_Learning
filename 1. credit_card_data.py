61ce9b56ed94d0f139f04d57c36c9ce4b947d754
ryoki_tanaka <ryoki.tanaka@rakus-partners.co.jp>
2021-06-19 20:16:39 +0900

    ファイルへの追記

---
+++ 1. credit_card_data.py
@@ -0,0 +1,33 @@
+# %% [markdown]
+# ## 機械学習システム Part1
+# %%
+import pandas as pd
+
+# データの読み込み
+data_row = pd.read_csv('./data/credit_card_data/creditcard.csv')
+# データの削減
+data_row.to_pickle('./data/credit_card_data/creditcard.pkl')
+# %%
+# pklデータでの読み込み
+data = pd.read_pickle('./data/credit_card_data/creditcard.pkl')
+data
+# %%
+# 要約統計量
+data.describe()
+# %%
+# データ列の確認
+data.columns
+# %%
+# データの欠損値確認
+data.isnull().sum()
+# %%
+# 固有の値（ユニークな値）の確認
+distinctCounter = data.apply(lambda x: len(x.unique()))
+distinctCounter
+# %% [markdown]
+# ## 不正なトランザクションを確認
+# 不正なトランザクション＝「Class列＝１」
+# %%
+print(f'不正なトランザクションの数：', data['Class'].sum())
+# %%
+# %%
